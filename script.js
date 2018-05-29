d00438be76bc4f6005f3629c623e66254bdde9f2
SergeySemendyaev <37715911+SergeySemendyaev@users.noreply.github.com>
2018-05-29 02:33:27 -0700

    sort works, but transition doesn't
    sort function works both ways ascending  and descending, but transition is ignored;

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-var array;
-var numbersArray;
-var ascending = true;
+var userInput;
+var digitsFromUserInput;
+var ascendingOrder = true;
 var div = document.getElementsByClassName("list")[0];
-var marginLeft = 5;
-
+var spans = document.getElementsByTagName("span");
+var button = document.getElementsByTagName("button")[0];
+var positionLeft;
+var positions;
 
 function makeNumbers(array) {
+    positions = [];
     var result = [];
-    div.innerHTML = "";
     for (var i = 0; i < array.length; i++) {
-        if (!Number.isInteger(+array[i]))
+        if (!Number.isInteger(+array[i])|| array[i]==" ")
             continue;
         result.push(array[i]);
         var span = document.createElement("span");
-        span.className = [i];
-        span.style.position = "absolute";
-        span.style.marginLeft = marginLeft+"px";
-        marginLeft += 15;
+        span.style.position = "relative";
+        span.style.left = positionLeft + "px";
+        positions.push(positionLeft);
+        positionLeft += 15;
         span.innerHTML = array[i];
         div.appendChild(span);
     }
@@ -24,44 +26,62 @@ function makeNumbers(array) {
 }
 
 function getInput() {
-    array = document.getElementsByTagName("input")[0].value;
-    numbersArray = makeNumbers(array);
-    document.getElementsByTagName("input")[0].value = numbersArray.join("");
+    clearDiv()
+    userInput = document.getElementsByTagName("input")[0].value;
+    digitsFromUserInput = makeNumbers(userInput);
+    document.getElementsByTagName("input")[0].value = digitsFromUserInput.join("");
 }
-function sort(){
-    if(ascending)
+
+function clearDiv(){
+    for(var i = 0; i < spans.length; )
+        div.removeChild(spans[0]);
+    positionLeft = 5;
+}
+
+function sort() {
+    
+    if (ascendingOrder)
         sortAscending();
-    else 
+    else
         sortDescending();
 }
 
 function sortAscending() {
-    var i = numbersArray.length - 1;
-    for (; i > 0; i--) {
-        if (numbersArray[i - 1] > numbersArray[i]) {
-            var temp = numbersArray[i - 1];
-            numbersArray[i - 1] = numbersArray[i];
-            numbersArray[i] = temp;
-            div.innerHTML = numbersArray.join("");
+    for (var i = spans.length - 1; i > 0; i--) {
+        if (spans[i - 1].innerHTML > spans[i].innerHTML) {
+            spans[i - 1].style.color = spans[i].style.color = "red";
+            spans[i - 1].style.left = positions[i - 1] + 23 + "px";
+            spans[i-1].addEventListener('transitionend', swap(i));
+            spans[i - 1].style.left = positions[i - 1] + "px";
+            spans[i - 1].style.color = spans[i].style.color = "black";
             break;
         }
     }
-    if(i == 0) 
-        ascending = false;
+    if (i == 0){
+        ascendingOrder = false;
+        sort();
+    }
 }
 
-function sortDescending(){
-    var i = 0;
-    for (; i < numbersArray.length - 1; i++) {
-        if (numbersArray[i] < numbersArray[i+1]) {
-            var temp = numbersArray[i];
-            numbersArray[i] = numbersArray[i+1];
-            numbersArray[i+1] = temp;
-            var div = document.getElementsByClassName("list")[0];
-            div.innerHTML = numbersArray.join("");
+function swap(index){
+    var temp = spans[index - 1].innerHTML;
+    spans[index - 1].innerHTML = spans[index].innerHTML;
+    spans[index].innerHTML = temp;
+}
+
+function sortDescending() {
+    for (var i = 0; i < spans.length - 1; i++) {
+        if (spans[i].innerHTML < spans[i + 1].innerHTML) {
+            spans[i].style.color = spans[i+1].style.color = "red";
+            spans[i].style.left = positions[i] + 23 + "px";
+            button.addEventListener('transitionend', swap(i+1));
+            spans[i].style.left = positions[i]+"px";
+            spans[i].style.color = spans[i+1].style.color = "black";
             break;
         }
     }
-    if(i == numbersArray.length - 1)
-        ascending = true;
+    if (i == spans.length - 1){
+        ascendingOrder = true;
+        sort();
+    }
 }
