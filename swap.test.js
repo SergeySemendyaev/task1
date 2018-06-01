959760ef51948040464a45fda15c145f5c2ec1d1
SergeySemendyaev <37715911+SergeySemendyaev@users.noreply.github.com>
2018-06-01 04:26:01 +0300

    test of swap function

---
+++ swap.test.js
@@ -0,0 +1,15 @@
+var swap = require('./swap');
+
+test('swaps span1 and span2 id and style.left',()=>{
+    var span1 = document.createElement('span');
+    span1.id = 1;
+    span1.style.left = '10px';
+    var span2 = document.createElement('span');
+    span2.id = 2;
+    span2.style.left = '20px';
+    swap(span1, span2);
+    expect(span1.id).toBe('2');
+    expect(span2.id).toBe('1');
+    expect(span1.style.left).toBe('20px');
+    expect(span2.style.left).toBe('10px');
+});
